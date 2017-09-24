eed807e7f6365863c158ef247b48042b9eb19d93
isa <isaliangyiyun@hotmail.com>
2017-09-24 14:35:54 -0400

    add logitic regression related algos

--- coursera-assignments/machine-learning-ex2/ex2/costFunction.m
+++ coursera-assignments/machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,11 +20,14 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% hypothesis
+h = sigmoid(X * theta); % theta' * X
 
+% cost function J(i)
+J = 1/m * (-y' * log(h) - (1 - y') * log(1 - h));
 
-
-
-
+% gradient
+grad = 1/m * (X' * (h-y));
 
 
 % =============================================================
